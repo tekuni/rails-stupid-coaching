5b7ce9cfae9c9372852ab823c0cb7dd5a79b1c55
Tomoyuki Ekuni <tekuni@yahoo.com>
2018-11-05 15:21:55 +0900

    updated rails.

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,26 @@
+class QuestionsController < ApplicationController
+
+  # QUESTIONS = {
+
+  # }
+
+  # def index
+  #   @questions = params[:questions]
+  # end
+
+  def answer
+    # raise
+    @ask = params[:question]
+    if params[:question] == "I am going to work."
+      @answer = "Great!"
+    elsif params[:question].include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don\'t care, get dressed and go to work!"
+    end
+  end
+
+  def ask
+  end
+
+end
